f6d129e969c1022bc8949b28ac9bc34eabbce456
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-25 00:30:59 +0100

    Update feed.py

--- feed.py
+++ feed.py
@@ -14,7 +14,7 @@ fields = ["URN", 'Name', 'Title', 'Body', "Likes", "Comments", "React", "Repost"
 for col, field_name in zip(colms, fields):
     # header
     col.write(field_name)
-if feed_api() != None:
+if st.button('Start'):
     for x in feed_api():
         col1, col2, col3, col4, col5, col6, col7, col8 = st.columns((1, 2, 2, 2, 1, 1, 1, 1))
         col1.write(x)
