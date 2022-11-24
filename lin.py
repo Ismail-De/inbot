0ba996b12898827e7854afc05835ed3df6a9edb0
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-25 00:35:59 +0100

    Update lin.py

--- lin.py
+++ lin.py
@@ -110,13 +110,12 @@ def user_info():
 def feed_api():
   l = []
   api_url = 'https://api.linkedin.com/v2/activityFeeds?q=networkShares&count=50'
-  with st.form_submit_button(label = 'Start'):
-    response = requests.get(api_url, headers = hd())
-    response = response.json()
-    print(response)
-    for i in response["elements"]:
-      l+= [i["reference"]]
-    return l
+  response = requests.get(api_url, headers = hd())
+  response = response.json()
+  print(response)
+  for i in response["elements"]:
+    l+= [i["reference"]]
+  return l
 
 def repost(n, message = ''):
   api_url = 'https://api.linkedin.com/v2/ugcPosts'
