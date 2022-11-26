a8c0f4abd1033bb982c9984e55de59565ecffd86
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-26 21:23:04 +0100

    Update feed.py

--- feed.py
+++ feed.py
@@ -14,7 +14,7 @@ fields = ["URN", 'Name', 'Title', 'Body', "Likes", "Comments", "React", "Repost"
 for col, field_name in zip(colms, fields):
     # header
     col.write(field_name)
-for x in feed_api(bool(submitted)):
+for x in feed_api():
     col1, col2, col3, col4, col5, col6, col7, col8 = st.columns((1, 2, 2, 2, 1, 1, 1, 1))
     col1.write(x)
     col2.write(get_id(x))
