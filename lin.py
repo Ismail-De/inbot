9f0c7b2d4436d848cee65e8bc9cf9c3c2e1ba76c
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-27 17:07:14 +0100

    Update lin.py

--- lin.py
+++ lin.py
@@ -1,48 +1,8 @@
 import requests
 import json
 from urllib.parse import urlparse, parse_qs
-import streamlit as st
 import secrets
-
-client_id = '86wluahn9m77um'
-client_secret = 'Tp9m8CH7oGBklnCq'
-redirect_uri = 'https://api-university.com/'
-
-buttt = st.empty()
-def auth():
-  #file = open('token.txt', 'r')
-  with open('token.txt') as f:
-    first_line = f.readline()
-  print(first_line)
-  base_url = "https://www.linkedin.com/oauth/v2/authorization&quot"
-  scope = "w_member_social,r_liteprofile"
-  url = f"https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope={scope}"
-  if 'access_token' in first_line:
-    access_token = first_line[len('access_token: '):]
-    access_token = access_token.replace(" ", "")
-    access_token = access_token.replace("\n", "")
-    print('"'+str(access_token)+'"')
-    #file.close()
-    return access_token
-
-  else: 
-      args = client_id,client_secret,redirect_uri
-      st.write("Please visit this" + url)
-      with st.form("my_form"):
-        title = st.text_input('Paste the full redirect URL here: (Press Submit)')
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-          print(title)
-          auth_code = title
-          access_token = refresh_token(auth_code)
-          #file.close()
-          file = open('token.txt', 'w')
-          file.write('access_token: ')
-          file.write(auth_code)
-          file.write('\n')
-          file.close()
-          print(access_token)
-          return access_token
+from ln_oauth import auth, headers
 
 def headers(access_token):
   headers = {
@@ -73,14 +33,14 @@ def hd():
   return hds
 
 def user_info():
-  response = requests.get('https://api.linkedin.com/v2/me', headers = hd())
+  response = requests.get('https://api.linkedin.com/v2/me', headers = headers)
   user_info = response.json()
   return user_info['id']
 
 def feed_api():
   l = []
   api_url = 'https://api.linkedin.com/v2/activityFeeds?q=networkShares&count=50'
-  response = requests.get(api_url, headers = hd())
+  response = requests.get(api_url, headers = headers)
   response = response.json()
   print(response)
   for i in response["elements"]:
@@ -154,4 +114,6 @@ def comm(n: str, message = 'Thanks for sharing'):
       "text":message
    }
   }
+  requests.post(api_url, headers=headers, json=data)
+  return 
 
