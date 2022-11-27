0575b63c854d34dfef8f6036e249e53f23c1bb00
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-27 16:57:18 +0100

    Update lin.py

--- lin.py
+++ lin.py
@@ -28,20 +28,21 @@ def auth():
   else: 
       args = client_id,client_secret,redirect_uri
       st.write("Please visit this" + url)
-      title = st.text_input('Paste the full redirect URL here: (Press Submit)')
-      submitted = buttt.button("Submit")
-      if submitted:
-        print(title)
-        auth_code = title
-        access_token = refresh_token(auth_code)
-        #file.close()
-        file = open('token.txt', 'w')
-        file.write('access_token: ')
-        file.write(auth_code)
-        file.write('\n')
-        file.close()
-        print(access_token)
-        return access_token
+      with st.form("my_form"):
+        title = st.text_input('Paste the full redirect URL here: (Press Submit)')
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+          print(title)
+          auth_code = title
+          access_token = refresh_token(auth_code)
+          #file.close()
+          file = open('token.txt', 'w')
+          file.write('access_token: ')
+          file.write(auth_code)
+          file.write('\n')
+          file.close()
+          print(access_token)
+          return access_token
 
 def headers(access_token):
   headers = {
