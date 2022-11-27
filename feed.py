ba4cd59a1238cfc83ce83aae2fdaf846850a1b8c
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-27 17:14:57 +0100

    Update feed.py

--- feed.py
+++ feed.py
@@ -17,43 +17,41 @@ for col, field_name in zip(colms, fields):
     # header
     col.write(field_name)
 
-s = auth()
-if s != None: 
-    for x in feed_api():
-        col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns((1, 2, 2, 2, 1, 1, 1, 1, 2))
-        col1.write(x)
-        col2.write(get_id(x))
-        col3.write(get_desc_title(x)[0])
-        col4.write(get_desc_title(x)[1])   # email status
-        col5.write(tot_like_cmmt(x)[1])
-        col6.write(tot_like_cmmt(x)[0])
-        
-        reactt = 'React'
-        button_type = "React" if reactt else "Done"
-        button_phold = col7.empty()
-        m = x + 'r'
-        do_action = button_phold.checkbox(button_type, key=m)
-        if do_action:
-          react(x)
-          button_phold.empty()
-            
-        repostt = 'Repost'
-        button_type1 = "Repost" if repostt else "Done"
-        button_phold1 = col8.empty()
-        m = x + 're'
-        do_action1 = button_phold1.checkbox(button_type1, key=m)
-        if do_action1:
-          repost(x)
-          button_phold1.empty()
-            
-        comment = 'Comment'
-        button_type2 = "Comment" if comment else "Done"
-        button_phold2 = col9.empty()
-        m = x + 'c'
-        do_action2 = button_phold2.checkbox(button_type2, key=m)
-        if do_action2:
-          if cmmt!=None:
-              comment(x, message = cmmt)
-          else:
-             comment(x)
-          button_phold2.empty()
+for x in feed_api():
+    col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns((1, 2, 2, 2, 1, 1, 1, 1, 2))
+    col1.write(x)
+    col2.write(get_id(x))
+    col3.write(get_desc_title(x)[0])
+    col4.write(get_desc_title(x)[1])   # email status
+    col5.write(tot_like_cmmt(x)[1])
+    col6.write(tot_like_cmmt(x)[0])
+
+    reactt = 'React'
+    button_type = "React" if reactt else "Done"
+    button_phold = col7.empty()
+    m = x + 'r'
+    do_action = button_phold.checkbox(button_type, key=m)
+    if do_action:
+      react(x)
+      button_phold.empty()
+
+    repostt = 'Repost'
+    button_type1 = "Repost" if repostt else "Done"
+    button_phold1 = col8.empty()
+    m = x + 're'
+    do_action1 = button_phold1.checkbox(button_type1, key=m)
+    if do_action1:
+      repost(x)
+      button_phold1.empty()
+
+    comment = 'Comment'
+    button_type2 = "Comment" if comment else "Done"
+    button_phold2 = col9.empty()
+    m = x + 'c'
+    do_action2 = button_phold2.checkbox(button_type2, key=m)
+    if do_action2:
+      if cmmt!=None:
+          comment(x, message = cmmt)
+      else:
+         comment(x)
+      button_phold2.empty()
